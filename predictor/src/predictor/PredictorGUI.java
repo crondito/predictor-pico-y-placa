2503b8ca81ad7e8381fb9cb92fccf6e0dc009259
Carlos Rodas <crods57@gmail.com>
2020-10-09 18:56:16 -0500

    Done

--- predictor/src/predictor/PredictorGUI.java
+++ predictor/src/predictor/PredictorGUI.java
@@ -36,9 +36,85 @@ public class PredictorGUI extends javax.swing.JFrame {
     }
     
     private boolean licensePlateCheck(LocalDateTime date2, int number){
-        boolean go;
-        
-        return true;
+        boolean go = true;
+        int day = date2.getDayOfWeek().getValue();
+        LocalDateTime a = LocalDateTime.of(date2.getYear(), date2.getMonth(), date2.getDayOfMonth(), 7, 0);
+        LocalDateTime b = LocalDateTime.of(date2.getYear(), date2.getMonth(), date2.getDayOfMonth(), 9, 30);
+        LocalDateTime c = LocalDateTime.of(date2.getYear(), date2.getMonth(), date2.getDayOfMonth(), 16, 0);
+        LocalDateTime d = LocalDateTime.of(date2.getYear(), date2.getMonth(), date2.getDayOfMonth(), 19, 30);
+        switch(day) {
+        // Monday
+        case 1:
+            if(number == 1 || number == 2){
+                if ((date2.isAfter(a) || date2.equals(a)) && (date2.isBefore(b) || date2.equals(b))) {
+                    go = false;
+                } else if ((date2.isAfter(c) || date2.equals(c)) && (date2.isBefore(d) || date2.equals(d))){
+                    go = false;
+                }
+            } else {
+                go = true;
+            }
+          break;
+        // Tuesday
+        case 2:
+            if(number == 3 || number == 4){
+                if ((date2.isAfter(a) || date2.equals(a)) && (date2.isBefore(b) || date2.equals(b))) {
+                    go = false;
+                } else if ((date2.isAfter(c) || date2.equals(c)) && (date2.isBefore(d) || date2.equals(d))){
+                    go = false;
+                }
+            } else {
+                go = true;
+            }
+          break;
+        // Wednesday
+        case 3:
+            if(number == 5 || number == 6){
+                if ((date2.isAfter(a) || date2.equals(a)) && (date2.isBefore(b) || date2.equals(b))) {
+                    go = false;
+                } else if ((date2.isAfter(c) || date2.equals(c)) && (date2.isBefore(d) || date2.equals(d))){
+                    go = false;
+                }
+            } else {
+                go = true;
+            }
+          break;
+        // Thursday
+        case 4:
+            if(number == 7 || number == 8){
+                if ((date2.isAfter(a) || date2.equals(a)) && (date2.isBefore(b) || date2.equals(b))) {
+                    go = false;
+                } else if ((date2.isAfter(c) || date2.equals(c)) && (date2.isBefore(d) || date2.equals(d))){
+                    go = false;
+                }
+            } else {
+                go = true;
+            }
+          break;
+        // Friday
+        case 5:
+            if(number == 9 || number == 0){
+                if ((date2.isAfter(a) || date2.equals(a)) && (date2.isBefore(b) || date2.equals(b))) {
+                    go = false;
+                } else if ((date2.isAfter(c) || date2.equals(c)) && (date2.isBefore(d) || date2.equals(d))){
+                    go = false;
+                }
+            } else {
+                go = true;
+            }
+          break;
+        // Saturday
+        case 6:
+          go = true;
+          break;
+        // Sunday
+        case 7:
+          go = true;
+          break;
+        default:
+          go = true;
+      }
+        return go;
     }
     
     @SuppressWarnings("unchecked")
@@ -102,7 +178,7 @@ public class PredictorGUI extends javax.swing.JFrame {
         jLabel4.setText("Time");
 
         jFormattedTextField1.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("dd/MM/yyyy"))));
-        jFormattedTextField1.setToolTipText("dd/mm/yy | ex: 15/09/2019");
+        jFormattedTextField1.setToolTipText("dd/mm/yyyy | ex: 15/09/2019");
 
         jFormattedTextField2.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("HH:mm"))));
         jFormattedTextField2.setToolTipText("HH:mm | ex: 15:56");
@@ -192,7 +268,7 @@ public class PredictorGUI extends javax.swing.JFrame {
             for (int i = 0; i < str.length(); i++) {
                 ch[i] = str.charAt(i);
             }
-            // Validate if it is an Ecuadorian License Plate
+            // Validate if it is an Ecuadorian Car License Plate
             if (ch.length == 7){
                 if (
                     isALetter(Character.toString(ch[0]))== true &&
@@ -216,7 +292,7 @@ public class PredictorGUI extends javax.swing.JFrame {
                     int number = parseInt(Character.toString(ch[6]));
                     // Check the license plate
                     if (licensePlateCheck(date2, number) == true){
-                        jTextField2.setText("YOU MAY USE YOUR VEHICLE");
+                        jTextField2.setText("YOU CAN USE YOUR VEHICLE");
                         jTextField2.setBackground(Color.green);
                     } else {
                         jTextField2.setText("YOU CANNOT USE YOUR VEHICLE");
